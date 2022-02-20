4143eabced0e4639c492def5fcecd908694621d2
Sergio Robledo <seergiorobledo@gmail.com>
2022-02-19 23:04:08 -0300

    some fixes

--- src/components/heroCity/HeroCity.jsx
+++ src/components/heroCity/HeroCity.jsx
@@ -3,9 +3,9 @@ import './heroCity.css'
 
 export default function HeroCity() {
     return (
-        <div className="hero">
-            <h2>Under Construction</h2>
-            <h3>Our website is currently undergoing scheduled maintenance. We should be back shortly. Thank you for your patience. </h3>
+        <div className="heroCity">
+            <h2 className="tittleh2">Under Construction</h2>
+            <h3 className="tittleh3">Our website is currently undergoing scheduled maintenance. We should be back shortly. Thank you for your patience. </h3>
         </div>
     )
 }
