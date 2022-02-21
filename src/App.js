ec64c045bf71faf5b5853e168ec352672e61348d
Sergio Robledo <seergiorobledo@gmail.com>
2022-02-21 02:24:04 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ResponsiveAppBar from './components/nav/Nav'
-// import Gallery from './components/gallery/Gallery'
-// import Hero from './components/hero/Hero.jsx'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-// import Cities from './components/Cities/Cities'
 import Footer from './components/footer/Footer'
-// import Comments from './components/reviews/Comments'
-// import Us from './components/us/Us'
-// import Cta from './components/cta/Cta'
 import Home from './Pages/Home'
 import Cities from './Pages/Cities'
-// import Cardreview from './components/cardreview/Cardreview';
 
 
 function App() {
@@ -18,10 +11,7 @@ function App() {
     <div>
       <BrowserRouter> 
       <ResponsiveAppBar/>
-      {/* <Hero/>
-      <Us/>
-      <Cta/>
-      <Comments/> */}
+      
       <Routes>
 
       <Route path="/" element={<Home/>}/>
