adf08dc07f817fa02f715ca1b2f5256323895db5
Sergio Robledo <seergiorobledo@gmail.com>
2022-03-28 21:35:20 -0300

    build

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const cors = require('cors')
 require('./config/database')
 const Router = require('./routes/routes')
 const passport = require('passport')
+// const PORT = 4000
+const app = express();
+const path = require('path');
 
-const PORT = 4000
-
-const app = express()
 
 //middleware
 
@@ -16,9 +16,15 @@ app.use(express.json());
 app.use(passport.initialize())
 app.use('/api', Router)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'))
+  });
+}
 
 
-app.listen(PORT, ()=>console.log('Server Ready on PORT '+PORT))
+app.listen( process.env.PORT || 4000, process.env.HOST || '0.0.0.0', ()=>console.log(`Server Ready on PORT + ${process.env.PORT || 4000}`))
 
 
 
